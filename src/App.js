91a837542596b3352eece879f69a3f14c25ae23f
Bruno Duarte <bruno.duarte411@gmail.com>
2022-10-21 18:21:50 -0300

    Add header mobile position

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
-import Logo from "./components/Logo"
+import Header from "./components/Header"
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <Logo />
+      <Header />
     </div>
   );
 }
---
+++ src/components/Header.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import "./Header.css"
+import Logo from "./Logo"
+
+const Header = () => {
+  return (
+    <header>
+        <Logo />
+    </header>
+  )
+}
+
+export default Header
